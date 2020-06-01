fd887bc87db2c0cff4bd45d38e4be42e791c0101
Adam Jaamour <adam@jamour.com>
2020-06-01 18:33:11 +0200

    Don't use pytorch for now

--- code/src/main.py
+++ code/src/main.py
@@ -1,8 +1,7 @@
 import socket
 
-import numpy as np
 import tensorflow as tf
-import torch
+# import torch
 
 
 def main() -> None:
@@ -16,8 +15,8 @@ def main() -> None:
 
 def basic_import_test() -> None:
     # PyTorch test
-    x = torch.rand(5, 3)
-    print(x)
+    # x = torch.rand(5, 3)
+    # print(x)
 
     # TensorFlow test
     mnist = tf.keras.datasets.mnist
@@ -38,11 +37,11 @@ def basic_import_test() -> None:
 
 def test_gpu() -> None:
     # PyTorch
-    print("torch.cuda.current_device() ", torch.cuda.current_device())
-    print("torch.cuda.device(0) ", torch.cuda.device(0))
-    print("torch.cuda.device_count() ", torch.cuda.device_count())
-    print("torch.cuda.get_device_name(0) ", torch.cuda.get_device_name(0))
-    print("torch.cuda.is_available() ", torch.cuda.is_available())
+    # print("torch.cuda.current_device() ", torch.cuda.current_device())
+    # print("torch.cuda.device(0) ", torch.cuda.device(0))
+    # print("torch.cuda.device_count() ", torch.cuda.device_count())
+    # print("torch.cuda.get_device_name(0) ", torch.cuda.get_device_name(0))
+    # print("torch.cuda.is_available() ", torch.cuda.is_available())
 
     # Tensorflow
     print("GPUs Available: ", tf.config.experimental.list_physical_devices('GPU'))
