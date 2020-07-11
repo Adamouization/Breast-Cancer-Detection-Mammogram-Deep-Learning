0e2f8264e5f455613c11a98321a3f73423eec012
Adamouization <adam@jamour.com>
2020-07-11 11:37:09 +0100

    Use Jupyter Lab for remote coding

--- code/bash_scripts/remote-jupyter-setup.sh
+++ code/bash_scripts/remote-jupyter-setup.sh
@@ -4,4 +4,4 @@
 echo "Setting Dissertation remote Jupyter environment"
 source /cs/scratch/agj6/tf2/venv/bin/activate
 cd  ~/Projects/Breast-Cancer-Detection-and-Segmentation
-jupyter notebook --no-browser --port=8888
+jupyter lab --no-browser --port=8888
